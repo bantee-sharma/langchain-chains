ea2813049927cb5634b547ef51580a58981f4c19
bantee-sharma <banteesharma9@gmail.com>
2025-06-10 20:45:56 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,8 +1,21 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
-
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model = "gemini-2.0-flash")
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template="Generate 5 interesting facts about {topic}",
+    input_variables=["topic"]
+)
+
+chain = prompt | llm | parser
+
+res = chain.invoke("Cricket")
+print(res)
+
+
